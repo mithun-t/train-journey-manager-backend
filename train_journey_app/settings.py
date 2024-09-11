eafb04afb2580c637c47de04179a229b9e4d201f
Happy Tom <mithunaksh01@gmail.com>
2024-09-11 22:30:14 +0530

    allowed hosting added

--- train_journey_app/settings.py
+++ train_journey_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+k&lm#!_td+d-6f!61-6f*ngk-xks5-m6lpl5p+ieg8^8z2ub2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.41', '127.0.0.1', 'localhost']
 
 
 # Application definition
